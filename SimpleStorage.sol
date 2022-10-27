1f877f405db5e4670579473ca67360aff82ea16c
ozkrleal <ozkralm10@hotmail.com>
2022-10-27 05:34:46 -0500

    Lesson 5: Ethers.js Simple -  Contracts Ethers.js

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8; // 0.8.12    ^ for this verison and all 0.8s above OR >=0.8.7 <0.9.0
+pragma solidity ^0.8.7; // 0.8.12    ^ for this verison and all 0.8s above OR >=0.8.7 <0.9.0
 
 // EVM ETHEREUM VIRTUAL MACHINE
 // Avax, fantom, polygon
--- deploy.js
+++ deploy.js
@@ -1,7 +1,46 @@
-function main() {
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
   console.log("HI");
   let variable = 5;
+
   console.log(variable);
+
+  // http://0.0.0.0:8545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  const wallet = new ethers.Wallet(
+    "b5d50e85eccb5169575ff1faacc0f3abfabfacaedca7776a4ea74c13fd8f68bc",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract =
+    await contractFactory.deploy(/*{ gasPrice: 1000000000000 }*/);
+  await contract.deployTransaction.wait(1);
+
+  // get numbers
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+  const transactionResponse = await contract.store("7");
+  const transactionReceipt = await transactionResponse.wait(1);
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated Favorite Number: ${updatedFavoriteNumber}`);
 }
 
-main();
+main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
