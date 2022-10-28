dcaeab2c888f6baafa8d6c4c71c2cd8229f03400
ozkrleal <ozkralm10@hotmail.com>
2022-10-28 01:37:36 -0500

    Lesson 5 Completed: Ethers.js Simple Storage

--- deploy.ts
+++ deploy.ts
@@ -1,6 +1,10 @@
-const ethers = require("ethers")
-const fs = require("fs-extra")
-require("dotenv").config()
+// const ethers = require("ethers")
+// const fs = require("fs-extra")
+// require("dotenv").config()
+
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main() {
     console.log("HI")
@@ -11,10 +15,10 @@ async function main() {
     // http://0.0.0.0:8545
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
 
-    //const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
     //const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
-    let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-    wallet = await wallet.connect(provider)
+    // let wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+    // wallet = await wallet.connect(provider)
 
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
     const binary = fs.readFileSync(
